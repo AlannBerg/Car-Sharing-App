dbb5bba0e52f679d8da16ea27529dd3b5f0c232b
ALan Berg <alan@berg@wp.pl>
2022-01-26 12:33:15 +0100

    DONE

--- src/main/java/com/example/CarRentalAplication/Security/MyUserDetailService.java
+++ src/main/java/com/example/CarRentalAplication/Security/MyUserDetailService.java
@@ -30,6 +30,7 @@ public class MyUserDetailService implements UserDetailsService {
             throw new InvalidUserName();
         }
 
+
         return new MyUserDetails(clients.get(0));
     }
 
